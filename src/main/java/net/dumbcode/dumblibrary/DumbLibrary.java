f20d918138e220e79b3bb706a102d6beddc70486
Wyn Price <wynprice999@gmail.com>
2018-07-22 15:31:24 +0100

    Version bumb (really need to automate this)

--- src/main/java/net/dumbcode/dumblibrary/DumbLibrary.java
+++ src/main/java/net/dumbcode/dumblibrary/DumbLibrary.java
@@ -12,7 +12,7 @@ public class DumbLibrary
 {
     public static final String MODID = "dumblibrary";
     public static final String NAME = "Dumb Library";
-    public static final String VERSION = "0.0.2";
+    public static final String VERSION = "0.1.0";
 
     private static Logger logger;
 

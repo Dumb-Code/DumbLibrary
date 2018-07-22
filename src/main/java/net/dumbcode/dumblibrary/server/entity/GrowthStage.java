0ba55f67af54ad769965e7c369bd28e42e2c6826
Wyn Price <wynprice999@gmail.com>
2018-07-22 15:19:12 +0100

    Re-ordered GrowthStage

--- src/main/java/net/dumbcode/dumblibrary/server/entity/GrowthStage.java
+++ src/main/java/net/dumbcode/dumblibrary/server/entity/GrowthStage.java
@@ -4,5 +4,5 @@ package net.dumbcode.dumblibrary.server.entity;
  * The growth stage
  */
 public enum GrowthStage {
-    ADULT, INFANT, JUVENILE, /*fluorescent*/ADOLESCENT, SKELETON
+    INFANT, JUVENILE, /*fluorescent*/ADOLESCENT, ADULT, SKELETON
 }

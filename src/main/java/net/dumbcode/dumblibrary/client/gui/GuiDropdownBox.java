e01e7232c45979bd57ebf5bbf7c8d8c0a30dc713
Wyn Price <wynprice999@gmail.com>
2018-10-26 16:48:00 +0100

    Vastly improved dropdown box

--- src/main/java/net/dumbcode/dumblibrary/client/gui/GuiDropdownBox.java
+++ src/main/java/net/dumbcode/dumblibrary/client/gui/GuiDropdownBox.java
@@ -1,11 +1,14 @@
 package net.dumbcode.dumblibrary.client.gui;
 
+import com.google.common.collect.Lists;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.util.ChatAllowedCharacters;
 import net.minecraft.util.math.MathHelper;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.GL11;
 
@@ -19,24 +22,26 @@ public class GuiDropdownBox {
 
     private static final float SCROLL_AMOUNT = 0.4F;
 
-    public final int width;
-    public final int cellHeight;
+    private final int width;
+    private final int cellHeight;
 
-    public final int cellMax;
+    private final int cellMax;
 
-    public final int xPos;
-    public final int yPos;
+    private final int xPos;
+    private final int yPos;
 
-    public boolean open;
+    private boolean open;
     private float scroll;
 
-    public SelectListEntry active;
+    private String search = "";
 
-    private final Supplier<List<SelectListEntry>> listSupplier;
+    private SelectListEntry active;
+
+    private final Supplier<List<? extends SelectListEntry>> listSupplier;
 
     private int lastYClicked = -1;
 
-    public GuiDropdownBox(int xPos, int yPos, int width, int cellHeight, int cellMax, Supplier<List<SelectListEntry>> listSupplier) {
+    public GuiDropdownBox(int xPos, int yPos, int width, int cellHeight, int cellMax, Supplier<List<? extends SelectListEntry>> listSupplier) {
         this.xPos = xPos;
         this.yPos = yPos;
         this.width = width;
@@ -46,22 +51,22 @@ public class GuiDropdownBox {
     }
 
     public void render(int mouseX, int mouseY) {
-        List<SelectListEntry> entries = this.listSupplier.get();
+        List<? extends SelectListEntry> entries = this.getSearchedList();
 
         int height = this.cellHeight + (this.open ?  Math.min(entries.size(), this.cellMax) * this.cellHeight : 0);
         int totalHeight = height - this.cellHeight;
 
         float scrollLength = -1;
         float scrollYStart = -1;
-        int scrollBarWidth = 6;
+        int scrollBarWidth = 8;
         int scrollBarLeft = this.xPos + this.width - scrollBarWidth;
 
         if(entries.size() > this.cellMax) {
             int ySize = (entries.size() - this.cellMax) * this.cellHeight;
-            scrollLength = MathHelper.clamp(totalHeight * totalHeight / ySize, 32, totalHeight - 8);
-            scrollYStart = this.scroll * this.cellHeight * (totalHeight - scrollLength) / (Math.max((entries.size() -  this.cellMax) * this.cellHeight, 0)) + this.yPos + this.cellHeight;
-            if (scrollYStart < this.yPos) {
-                scrollYStart = this.yPos;
+            scrollLength = MathHelper.clamp(totalHeight / ySize, 32, totalHeight - 8);
+            scrollYStart = this.scroll * this.cellHeight * (totalHeight - scrollLength) / (Math.max((entries.size() -  this.cellMax) * this.cellHeight, 0)) + this.yPos + this.cellHeight - 1;
+            if (scrollYStart < this.yPos - 1) {
+                scrollYStart = this.yPos - 1;
             }
         }
 
@@ -113,9 +118,9 @@ public class GuiDropdownBox {
 
         if(this.open) {
             for (int i = 0; i < entries.size(); i++) {
-                int yStart = (int) (this.yPos + this.cellHeight * (i + 1) - this.scroll * this.cellHeight);
+                int yStart = (int) (this.yPos + this.cellHeight * (i + 1) - this.scroll * this.cellHeight) - 1;
                 //Usually it would be yStart + cellHeight, however because the ystart is offsetted (due to the active selection box), it cancels out
-                if(yStart + 5 >= this.xPos && yStart - 5 <= this.xPos + height) {
+                if(yStart >= this.yPos && yStart <= this.yPos + height) {
                     Gui.drawRect(this.xPos, yStart, this.xPos + this.width, yStart + this.cellHeight, insideSelectionColor);
                     Gui.drawRect(this.xPos, yStart, this.xPos + this.width, yStart + borderSize, borderColor);
                     entries.get(i).draw(this.xPos, yStart);
@@ -129,7 +134,7 @@ public class GuiDropdownBox {
             if(relX <= this.width){
                 if (relY <= this.cellHeight) {
                     Gui.drawRect(this.xPos, this.yPos, this.xPos + this.width, this.yPos + this.cellHeight, highlightColor);
-                } else if(this.open) {
+                } else if(relY < height && this.open) {
                     if(entries.size() > this.cellMax && mouseX >= scrollBarLeft && mouseX <= scrollBarLeft + scrollBarWidth && mouseY >= scrollYStart && mouseY <= scrollYStart + scrollLength) {
                         highlighedScrollbar = true;
                     } else if(this.lastYClicked == -1) {
@@ -171,12 +176,15 @@ public class GuiDropdownBox {
         Gui.drawRect(this.xPos, this.yPos, this.xPos + this.width, this.yPos + this.cellHeight, insideColor);
 
 
-        if(this.active != null) {
+        if(!this.search.isEmpty()) {
+            mc.fontRenderer.drawString(this.search, this.xPos + 5, this.yPos + this.cellHeight / 2 - mc.fontRenderer.FONT_HEIGHT / 2, -1);
+        } else if(this.active != null) {
             this.active.draw(this.xPos, this.yPos);
         }
 
         Gui.drawRect(this.xPos, this.yPos, this.xPos + this.width, this.yPos + borderSize, borderColor);
         Gui.drawRect(this.xPos, this.yPos + height, this.xPos + this.width, this.yPos + height - borderSize, borderColor);
+        Gui.drawRect(this.xPos, this.yPos + cellHeight, this.xPos + this.width, this.yPos + cellHeight - borderSize, borderColor);
         Gui.drawRect(this.xPos, this.yPos, this.xPos + borderSize, this.yPos + height, borderColor);
         Gui.drawRect(this.xPos + this.width, this.yPos, this.xPos + this.width - borderSize, this.yPos + height, borderColor);
         GlStateManager.enableDepth();
@@ -184,22 +192,22 @@ public class GuiDropdownBox {
 
     public void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
         if(mouseButton == 0) {
-            List<SelectListEntry> entries = this.listSupplier.get();
+            List<? extends SelectListEntry> entries = this.getSearchedList();
 
             int height = this.cellHeight + (this.open ?  Math.min(entries.size(), this.cellMax) * this.cellHeight : 0);
             int totalHeight = height - this.cellHeight;
 
             float scrollLength = -1;
             float scrollYStart = -1;
-            int scrollBarWidth = 6;
+            int scrollBarWidth = 8;
             int scrollBarLeft = this.xPos + this.width - scrollBarWidth;
 
             if(entries.size() > this.cellMax) {
                 int ySize = (entries.size() - this.cellMax) * this.cellHeight;
-                scrollLength = MathHelper.clamp(totalHeight * totalHeight / ySize, 32, totalHeight - 8);
-                scrollYStart = this.scroll * this.cellHeight * (totalHeight - scrollLength) / (Math.max((entries.size() -  this.cellMax) * this.cellHeight, 0)) + this.yPos + this.cellHeight;
-                if (scrollYStart < this.yPos) {
-                    scrollYStart = this.yPos;
+                scrollLength = MathHelper.clamp(totalHeight / ySize, 32, totalHeight - 8);
+                scrollYStart = this.scroll * this.cellHeight * (totalHeight - scrollLength) / (Math.max((entries.size() -  this.cellMax) * this.cellHeight, 0)) + this.yPos + this.cellHeight - 1;
+                if (scrollYStart < this.yPos - 1) {
+                    scrollYStart = this.yPos - 1;
                 }
             }
 
@@ -215,7 +223,7 @@ public class GuiDropdownBox {
                     if(relY <= this.cellHeight) {
                         this.open = !this.open;
                         return;
-                    } else if(this.open){
+                    } else if(relY < height && this.open){
                         for (int i = 0; i < entries.size(); i++) {
                             if(relY <= this.cellHeight * (i + 2) - this.scroll * this.cellHeight) {
                                 entries.get(i).onClicked(relX, relY);
@@ -228,12 +236,30 @@ public class GuiDropdownBox {
             }
         }
         this.open = false;
+        this.search = "";
     }
 
     public void handleMouseInput() {
         int mouseInput = Mouse.getEventDWheel();
         if(mouseInput != 0) {
             this.scroll((mouseInput < 0 ? -1 : 1) * SCROLL_AMOUNT);
+        }
+    }
+
+    public void handleKeyboardInput() {
+        if(!this.open) {
+            return;
+        }
+        char c = Keyboard.getEventCharacter();
+        if (Keyboard.getEventKey() == 0 && c >= ' ' || Keyboard.getEventKeyState()) {
+            if(Keyboard.getEventKey() == Keyboard.KEY_BACK) {
+                if(!this.search.isEmpty()) {
+                    this.search = this.search.substring(0, this.search.length() - 1);
+                }
+            } else if(ChatAllowedCharacters.isAllowedCharacter(c)) {
+                this.search += Character.toLowerCase(c);
+                this.scroll = 0;
+            }
         }
     }
 
@@ -245,24 +271,43 @@ public class GuiDropdownBox {
                 if(relY <= this.cellHeight) {
                     return true;
                 } else if(this.open){
-                    return relY <= this.cellHeight * (Math.min(this.listSupplier.get().size(), this.cellMax) + 1);
+                    return relY <= this.cellHeight * (Math.min(this.getSearchedList().size(), this.cellMax) + 1);
                 }
             }
         }
         return false;
     }
 
+    private List<? extends SelectListEntry> getSearchedList() {
+        if(this.search.isEmpty()) {
+            return this.listSupplier.get();
+        }
+        List<SelectListEntry> list = Lists.newArrayList();
+        for (SelectListEntry listEntry : this.listSupplier.get()) {
+            if(listEntry.getSearch().toLowerCase().contains(this.search)) {
+                list.add(listEntry);
+            }
+        }
+        return list;
+    }
+
     public void scroll(float amount) {
         this.scroll -= amount;
-        this.scroll = MathHelper.clamp(this.scroll, 0, Math.max(this.listSupplier.get().size() -  this.cellMax, 0));
+        this.scroll = MathHelper.clamp(this.scroll, 0, Math.max(this.getSearchedList().size() -  this.cellMax, 0));
     }
 
     public SelectListEntry getActive() {
         return this.active;
     }
 
+    public void setActive(SelectListEntry active) {
+        this.active = active;
+    }
+
     interface SelectListEntry {
         void draw(int x, int y);
+
+        String getSearch();
 
         default void onClicked(int relMouseX, int relMouseY) {
 

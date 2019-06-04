cd94c91a22a17ccce180a7105fff34e9ce2f59b1
Wyn-Price <wynprice999@gmail.com>
2019-06-04 15:16:25 +0100

    Added changelog commit?

---
+++ changelog.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+setup_git() {
+  
+  git clone https://github.com/Dumb-Code/Changelogs.git
+  
+  cd Changelogs
+  
+  git remote rm origin
+  git remote add origin https://Travis-CI:${GH_TOKEN}@github.com/Dumb-Code/Changelogs.git
+  
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+  git config --global commit.gpgsign false
+ 
+}
+
+edit_file() {
+  echo "$COMMITTER_NAME ($COMMITTER_EMAIL) @ $TRAVIS_COMMIT" >> dumb_library.txt
+  echo "    $TRAVIS_COMMIT_MESSAGE" >> dumb_library.txt
+  echo "" >> dumb_library.txt
+}
+
+push_files() {
+  git add dumb_library.txt
+  git commit --message "Dumb Library Update: $TRAVIS_BUILD_NUMBER:$TRAVIS_COMMIT"
+  git push origin master
+}
+
+setup_git
+edit_file
+push_files

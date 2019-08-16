981fad4c4f3f27370a22fa4da4b2b12c92a8f0f5
Wyn-Price <wynprice999@gmail.com>
2019-08-16 23:25:34 +0100

    Fixed serializing issue

--- src/main/java/net/dumbcode/dumblibrary/server/ecs/blocks/components/GrowingComponent.java
+++ src/main/java/net/dumbcode/dumblibrary/server/ecs/blocks/components/GrowingComponent.java
@@ -8,8 +8,10 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 import net.dumbcode.dumblibrary.server.ecs.component.EntityComponent;
 import net.dumbcode.dumblibrary.server.ecs.component.EntityComponentStorage;
+import net.dumbcode.dumblibrary.server.utils.IOCollectors;
 import net.minecraft.util.JsonUtils;
 
+import java.util.Arrays;
 import java.util.stream.StreamSupport;
 
 public class GrowingComponent implements EntityComponent {
@@ -31,16 +33,14 @@ public class GrowingComponent implements EntityComponent {
         @Override
         public void readJson(JsonObject json) {
             this.growTo = StreamSupport.stream(JsonUtils.getJsonArray(json, "grow_to").spliterator(), false)
-                    .filter(JsonElement::isJsonPrimitive)
-                    .map(JsonElement::getAsJsonPrimitive)
-                    .filter(JsonPrimitive::isString)
-                    .map(JsonPrimitive::getAsString)
+                    .filter(e -> e.isJsonPrimitive() && e.getAsJsonPrimitive().isString())
+                    .map(e -> e.getAsJsonPrimitive().getAsString())
                     .toArray(String[]::new);
         }
 
         @Override
         public void writeJson(JsonObject json) {
-
+            json.add("grow_to", Arrays.stream(this.growTo).map(JsonPrimitive::new).collect(IOCollectors.toJsonArray()));
         }
 
         public Storage setGrowTo(String... growTo) {

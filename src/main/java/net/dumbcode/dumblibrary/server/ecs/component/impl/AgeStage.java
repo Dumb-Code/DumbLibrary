ed8633901f52302be2af254cc16afc0ba388a424
Wyn-Price <wynprice999@gmail.com>
2019-12-05 21:12:41 +0000

    Don't let children breed yes

--- src/main/java/net/dumbcode/dumblibrary/server/ecs/component/impl/AgeStage.java
+++ src/main/java/net/dumbcode/dumblibrary/server/ecs/component/impl/AgeStage.java
@@ -1,8 +1,11 @@
 package net.dumbcode.dumblibrary.server.ecs.component.impl;
 
+import lombok.Data;
 import lombok.Value;
+import lombok.experimental.Accessors;
 
-@Value
+@Data
+@Accessors(chain = true)
 public class AgeStage {
 
     public static final AgeStage MISSING = new AgeStage("missing", -1, "missing");
@@ -10,4 +13,5 @@ public class AgeStage {
     private final String name;
     private final int time;
     private final String modelStage;
+    private boolean canBreed = false;
 }
--- src/main/java/net/dumbcode/dumblibrary/server/ecs/component/impl/FamilyComponent.java
+++ src/main/java/net/dumbcode/dumblibrary/server/ecs/component/impl/FamilyComponent.java
@@ -4,10 +4,14 @@ import com.google.gson.JsonObject;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import net.dumbcode.dumblibrary.server.ecs.ComponentAccess;
 import net.dumbcode.dumblibrary.server.ecs.FamilySavedData;
 import net.dumbcode.dumblibrary.server.ecs.component.EntityComponent;
 import net.dumbcode.dumblibrary.server.ecs.component.EntityComponentStorage;
+import net.dumbcode.dumblibrary.server.ecs.component.EntityComponentTypes;
+import net.dumbcode.dumblibrary.server.ecs.component.additionals.CanBreedComponent;
 import net.dumbcode.dumblibrary.server.json.JsonUtil;
+import net.minecraft.entity.Entity;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.JsonUtils;
 import net.minecraft.util.ResourceLocation;
@@ -16,22 +20,24 @@ import java.util.Optional;
 import java.util.UUID;
 
 
-public class FamilyComponent extends EntityComponent {
+public class FamilyComponent extends EntityComponent implements CanBreedComponent {
     @Getter @Setter private UUID familyUUID;
     @Getter @Setter private ResourceLocation familyTypeId;
+    private boolean mateForLife;
     private FamilySavedData dataCache;
 
-    public Optional<FamilySavedData> getDataCache() {
+    public FamilySavedData getDataCache() {
         if(this.dataCache == null) {
             this.dataCache = FamilySavedData.getData(this.familyUUID);
         }
-        return Optional.of(this.dataCache);
+        return this.dataCache;
     }
 
     @Override
     public NBTTagCompound serialize(NBTTagCompound compound) {
         compound.setUniqueId("family_uuid", this.familyUUID);
         compound.setString("family_type", this.familyTypeId.toString());
+        compound.setBoolean("mate_for_life", this.mateForLife);
         return super.serialize(compound);
     }
 
@@ -39,7 +45,18 @@ public class FamilyComponent extends EntityComponent {
     public void deserialize(NBTTagCompound compound) {
         this.familyUUID = compound.getUniqueId("family_uuid");
         this.familyTypeId = new ResourceLocation(compound.getString("family_type"));
+        this.mateForLife = compound.getBoolean("mate_for_life");
         super.deserialize(compound);
+    }
+
+    @Override
+    public boolean canBreedWith(ComponentAccess otherEntity) {
+        return otherEntity.get(EntityComponentTypes.FAMILY).map(f -> {
+            if(this.mateForLife && !this.familyUUID.equals(f.familyUUID)) {
+                return false;
+            }
+            return !this.getDataCache().getChildren().contains(((Entity)otherEntity).getUniqueID());
+        }).orElse(true);
     }
 
     @Getter

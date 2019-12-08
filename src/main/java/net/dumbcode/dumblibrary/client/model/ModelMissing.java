42b05735c29d19bd749639bcdbcb6b1f7c4ef167
Wyn-Price <wynprice999@gmail.com>
2019-12-08 01:42:10 +0000

    Moved missing model to extend tabula model.

--- src/main/java/net/dumbcode/dumblibrary/client/model/ModelMissing.java
+++ src/main/java/net/dumbcode/dumblibrary/client/model/ModelMissing.java
@@ -1,5 +1,7 @@
 package net.dumbcode.dumblibrary.client.model;
 
+import net.dumbcode.dumblibrary.client.model.tabula.TabulaModel;
+import net.dumbcode.dumblibrary.server.tabula.TabulaModelInformation;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
@@ -7,13 +9,14 @@ import net.minecraft.entity.Entity;
 /**
  * A simple missing model. Just one 16x16x16 cube
  */
-public class ModelMissing extends ModelBase {
+public class ModelMissing extends TabulaModel {
 
     public static final ModelMissing INSTANCE = new ModelMissing();
 
     private ModelRenderer cube;
 
     private ModelMissing() {
+        super(TabulaModelInformation.MISSING);
         this.cube = new ModelRenderer(this, 0, 0);
         this.cube.addBox(-8F, 8F, -8F, 16, 16, 16, 0.0F);
     }

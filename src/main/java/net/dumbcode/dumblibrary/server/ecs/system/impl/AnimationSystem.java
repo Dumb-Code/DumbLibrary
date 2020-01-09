65d30940abd9a3080ce97266a7b79b5baf34eae1
Wyn-Price <wynprice999@gmail.com>
2020-01-09 21:20:13 +0000

    Fixed issue with animation system not running future animations clientside

--- src/main/java/net/dumbcode/dumblibrary/server/ecs/system/impl/AnimationSystem.java
+++ src/main/java/net/dumbcode/dumblibrary/server/ecs/system/impl/AnimationSystem.java
@@ -1,10 +1,17 @@
 package net.dumbcode.dumblibrary.server.ecs.system.impl;
 
+import net.dumbcode.dumblibrary.server.ecs.ComponentAccess;
 import net.dumbcode.dumblibrary.server.ecs.EntityManager;
 import net.dumbcode.dumblibrary.server.ecs.component.EntityComponentTypes;
 import net.dumbcode.dumblibrary.server.ecs.component.impl.AnimationComponent;
 import net.dumbcode.dumblibrary.server.ecs.system.EntitySystem;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public enum AnimationSystem implements EntitySystem {
     INSTANCE;
@@ -16,10 +23,27 @@ public enum AnimationSystem implements EntitySystem {
         this.components = manager.resolveFamily(EntityComponentTypes.ANIMATION).populateBuffer(EntityComponentTypes.ANIMATION, this.components);
     }
 
+    private void updateComponent(AnimationComponent<?> component) {
+        component.getFutureAnimations().removeIf(AnimationComponent.FutureAnimation::tick);
+    }
+
     @Override
     public void update(World world) {
         for (AnimationComponent<?> component : this.components) {
-            component.getFutureAnimations().removeIf(AnimationComponent.FutureAnimation::tick);
+            this.updateComponent(component);
+        }
+    }
+
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public void onClientWorldTick(TickEvent.ClientTickEvent event) {
+        World world = Minecraft.getMinecraft().world;
+        if(world != null && !Minecraft.getMinecraft().isGamePaused()) {
+            for (Entity entity : world.loadedEntityList) {
+                if(entity instanceof ComponentAccess) {
+                    ((ComponentAccess) entity).get(EntityComponentTypes.ANIMATION).ifPresent(this::updateComponent);
+                }
+            }
         }
     }
 }

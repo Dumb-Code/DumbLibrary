1bf58561532b6024255c41dec0725480e57aebe5
Wyn-Price <wynprice999@gmail.com>
2020-01-09 21:21:06 +0000

    Rewritten blockstateworker to be not stupid

--- src/main/java/net/dumbcode/dumblibrary/server/utils/BlockStateWorker.java
+++ src/main/java/net/dumbcode/dumblibrary/server/utils/BlockStateWorker.java
@@ -1,63 +1,121 @@
 package net.dumbcode.dumblibrary.server.utils;
 
-import com.google.common.collect.Lists;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityCreature;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ChunkCache;
+import net.minecraft.world.World;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 import java.util.function.BiPredicate;
-import java.util.function.Predicate;
 
-public class BlockStateWorker implements Runnable {
+public class BlockStateWorker {
+    public static final BlockStateWorker INSTANCE = new BlockStateWorker(1000, 10);
 
-    private final BiPredicate<IBlockState, BlockPos> predicate;
-    private final Entity positioned;
-    private final int radii;
-    private BlockPos previousPosition;
-    private boolean activated;
+    private static int counter;
 
+    private final Thread workerThread;
+    private final int iterationsPerRun;
+    private final int minIterations;
+    private final Queue<Task> newTasks = new ArrayDeque<>();
+    private final List<Task> tasks = new ArrayList<>();
 
-    private final List<BlockPos> results = Lists.newArrayList();
-
-    public BlockStateWorker(BiPredicate<IBlockState, BlockPos> predicate, Entity positioned, int radii) {
-        this.predicate = predicate;
-        this.positioned = positioned;
-        this.radii = radii;
+    public BlockStateWorker(int iterationsPerRun, int minItertations) {
+        this.workerThread = new Thread(this::run, "BlockStateWorker-" + counter++);
+        this.workerThread.setDaemon(true);
+        this.workerThread.start();
+        this.iterationsPerRun = iterationsPerRun;
+        this.minIterations = minItertations;
     }
 
-    @Override
-    public void run() {
-        while(this.results.isEmpty()) {
-            if(this.previousPosition == null || this.previousPosition.distanceSq(this.positioned.getPosition()) > 25) {
-                for (int x = -this.radii; x < this.radii; x++) {
-                    for (int y = -this.radii; y < this.radii; y++) {
-                        for (int z = -this.radii; z < this.radii; z++) {
-                            BlockPos pos = new BlockPos(x + this.positioned.posX, y + this.positioned.posY, z + this.positioned.posZ);
-                            if(this.predicate.test(this.positioned.world.getBlockState(pos), pos)) {
-                                this.results.add(pos);
-                            }
-                        }
-                    }
+    private void run() {
+        while(!this.workerThread.isInterrupted()) {
+            synchronized (this.newTasks) {
+                while (!this.newTasks.isEmpty()) {
+                    this.tasks.add(this.newTasks.poll());
                 }
-                this.previousPosition = this.positioned.getPosition();
+            }
+            synchronized (this.tasks) {
+                if(!this.tasks.isEmpty()) {
+                    int iterationsPerTask = Math.max(this.iterationsPerRun / this.tasks.size(), this.minIterations);
+                    this.tasks.removeIf(t -> {
+                        t.runTask(iterationsPerTask);
+                        if(t.finished) {
+                            t.completableFuture.complete(t.result);
+                            return true;
+                        }
+                        return false;
+                    });
+                }
             }
         }
     }
 
-    public void activate() {
-        this.activated = true;
-        Thread thread = new Thread(this, "");
-        thread.setDaemon(true);
+    public Future<List<BlockPos>> runTask(World world, BlockPos center, int radius, BiPredicate<IBlockState, BlockPos> predicate) {
+        CompletableFuture<List<BlockPos>> future = new CompletableFuture<>();
+        synchronized (this.newTasks) {
+            this.newTasks.add(new Task(world, center, radius, predicate, future));
+        }
+        return future;
     }
 
-    public boolean isActivated() {
-        return this.activated;
-    }
 
-    public synchronized List<BlockPos> getResults() {
-        return this.results;
+    private static class Task {
+        private final ChunkCache world;
+        private final BiPredicate<IBlockState, BlockPos> predicate;
+        private final CompletableFuture<List<BlockPos>> completableFuture;
+        private final BlockPos min;
+        private final BlockPos max;
+        private final List<BlockPos> result = new ArrayList<>();
+
+        private boolean finished;
+        private int x;
+        private int y;
+        private int z;
+
+        private Task(World world, BlockPos center, int radii, BiPredicate<IBlockState, BlockPos> predicate, CompletableFuture<List<BlockPos>> completableFuture) {
+            this.predicate = predicate;
+            this.completableFuture = completableFuture;
+
+            this.min = center.add(-radii, -radii, -radii);
+            this.max = center.add(radii, radii, radii);
+
+            this.world = new ChunkCache(world, this.min, this.max, 0);
+
+            this.x = this.min.getX();
+            this.y = this.min.getY();
+            this.z = this.min.getZ();
+        }
+
+        private void runTask(int iterations) {
+            for (int i = 0; i < iterations; i++) {
+                BlockPos pos = new BlockPos(this.x, this.y, this.z);
+                IBlockState state = this.world.getBlockState(pos);
+                if(this.predicate.test(state, pos)) {
+                    this.result.add(pos);
+                }
+                if(this.increment()) {
+                    this.finished = true;
+                }
+            }
+        }
+
+        private boolean increment() {
+            this.x++;
+            if(this.x > this.max.getX()) {
+                this.x = this.min.getX();
+                this.y++;
+                if(this.y > this.max.getY()) {
+                    this.y = this.min.getY();
+                    this.z++;
+                    return this.z > this.max.getZ();
+                }
+            }
+            return false;
+        }
     }
 }

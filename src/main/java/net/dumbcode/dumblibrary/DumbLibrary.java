5da5e4b5e7d590f52348dcf5cf925cd6cac98ab6
wynprice999@gmail.com <wynprice999@gmail.com>
2020-04-26 01:35:04 +0100

    Version bump

--- src/main/java/net/dumbcode/dumblibrary/DumbLibrary.java
+++ src/main/java/net/dumbcode/dumblibrary/DumbLibrary.java
@@ -38,7 +38,7 @@ import org.apache.logging.log4j.Logger;
 public class DumbLibrary {
     public static final String MODID = "dumblibrary";
     public static final String NAME = "Dumb Library";
-    public static final String VERSION = "0.3.0";
+    public static final String VERSION = "0.4.0";
 
     private static Logger logger;
 

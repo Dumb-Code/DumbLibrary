82cd18c90c0d2e09e8a9564e1f89d64b4c19f3b4
wynprice999@gmail.com <wynprice999@gmail.com>
2020-05-10 06:32:38 +0100

    Added better helper method for tinting

--- src/main/java/net/dumbcode/dumblibrary/server/dna/EntityGeneticRegistry.java
+++ src/main/java/net/dumbcode/dumblibrary/server/dna/EntityGeneticRegistry.java
@@ -16,7 +16,7 @@ public enum EntityGeneticRegistry {
 
     EntityGeneticRegistry() {
         register(EntityBat.class, GeneticTypes.SIZE, new GeneticFieldModifierStorage().setOperation(ModOp.MULTIPLY_BASE_THEN_ADD), -0.25F);
-        register(EntityBat.class, GeneticTypes.OVERALL_TINT, new GeneticTypeOverallTintStorage().setTintType(GeneticTypeOverallTintStorage.TintType.TARGET), GeneticUtils.encode3BitColor(28/255F, 25/255F, 18/255F));
+        registerTargetTint(EntityBat.class, 0x1C1912);
     }
 
     private final Map<Class<? extends Entity>, List<Entry<?>>> entityEntryList = new HashMap<>();
@@ -27,6 +27,16 @@ public enum EntityGeneticRegistry {
 
     public <S extends GeneticFactoryStorage> void register(Class<? extends Entity> clazz, GeneticType<S> type, S storage, float value) {
         this.entityEntryList.computeIfAbsent(clazz, c -> new ArrayList<>()).add(new Entry<>(type, storage, value));
+    }
+
+    public void registerTargetTint(Class<? extends Entity> clazz, int color) {
+        int r = (color >> 16) & 0xFF;
+        int g = (color >> 8) & 0xFF;
+        int b = color & 0xFF;
+        this.register(clazz, GeneticTypes.OVERALL_TINT,
+            new GeneticTypeOverallTintStorage().setTintType(GeneticTypeOverallTintStorage.TintType.TARGET),
+            GeneticUtils.encode3BitColor(r/255F, g/255F, b/255F)
+        );
     }
 
 

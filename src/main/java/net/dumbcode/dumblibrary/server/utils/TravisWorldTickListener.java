af5f3bc8f047c2589db7d2658168b970919597eb
wynprice999@gmail.com <wynprice999@gmail.com>
2020-06-14 01:56:19 +0100

    Fixed travis tick listener

--- src/main/java/net/dumbcode/dumblibrary/server/utils/TravisWorldTickListener.java
+++ src/main/java/net/dumbcode/dumblibrary/server/utils/TravisWorldTickListener.java
@@ -1,16 +1,21 @@
 package net.dumbcode.dumblibrary.server.utils;
 
 import net.dumbcode.dumblibrary.DumbLibrary;
+import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
+//This is used so once travis loads the world it doesn't go on forever
 @Mod.EventBusSubscriber(modid = DumbLibrary.MODID)
 public class TravisWorldTickListener {
     @SubscribeEvent
     public static void onWorldTick(TickEvent.WorldTickEvent event) {
-        System.out.println(System.getenv("TRAVIS"));
+        if("true".equals(System.getenv("TRAVIS"))) {
+            FMLCommonHandler.instance().getMinecraftServerInstance().initiateShutdown();
+        }
         MinecraftForge.EVENT_BUS.unregister(TravisWorldTickListener.class);
     }
 }

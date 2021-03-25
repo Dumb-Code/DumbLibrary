b6fa152f83f82ca6afed9f36c4f494ce210ab781
Wyn Price <wynprice999@gmail.com>
2021-03-25 13:41:12 +0000

    Delete old animation code.

--- src/main/java/net/dumbcode/dumblibrary/server/animation/EntityWithAnimation.java
+++
@@ -1,7 +0,0 @@
-package net.dumbcode.dumblibrary.server.animation;
-
-import net.dumbcode.dumblibrary.server.animation.container.AnimationContainerEntry;
-
-public interface EntityWithAnimation {
-    AnimationContainerEntry getAnimationEntry();
-}

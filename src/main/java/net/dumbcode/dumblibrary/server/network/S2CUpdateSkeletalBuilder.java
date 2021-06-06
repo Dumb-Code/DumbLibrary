dfc6934c5734fd6b13eeb88e6c526e245a4c4c3b
Wyn Price <wynprice999@gmail.com>
2021-06-06 08:27:00 +0100

    Update S2CUpdateSkeletalBuilder.java

--- src/main/java/net/dumbcode/dumblibrary/server/network/S2CUpdateSkeletalBuilder.java
+++ src/main/java/net/dumbcode/dumblibrary/server/network/S2CUpdateSkeletalBuilder.java
@@ -7,7 +7,6 @@ import net.minecraft.network.PacketBuffer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.network.NetworkEvent;
-import sun.jvm.hotspot.opto.Block;
 
 import java.util.function.Supplier;
 

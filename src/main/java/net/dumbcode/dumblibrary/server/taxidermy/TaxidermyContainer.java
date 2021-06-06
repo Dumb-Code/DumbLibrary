ef53236e913727ea1e6265bc9f8b569333325a0c
Wyn Price <wynprice999@gmail.com>
2021-06-06 01:59:42 +0100

    Update TaxidermyContainer.java

--- src/main/java/net/dumbcode/dumblibrary/server/taxidermy/TaxidermyContainer.java
+++ src/main/java/net/dumbcode/dumblibrary/server/taxidermy/TaxidermyContainer.java
@@ -3,6 +3,9 @@ package net.dumbcode.dumblibrary.server.taxidermy;
 import net.dumbcode.dumblibrary.DumbLibrary;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.container.Container;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.inventory.container.SimpleNamedContainerProvider;
+import net.minecraft.util.text.StringTextComponent;
 
 public class TaxidermyContainer extends Container {
 
@@ -20,5 +23,9 @@ public class TaxidermyContainer extends Container {
 
     public BaseTaxidermyBlockEntity getBlockEntity() {
         return blockEntity;
+    }
+
+    public static INamedContainerProvider create(BaseTaxidermyBlockEntity blockEntity) {
+        return new SimpleNamedContainerProvider((id, inv, p) -> new TaxidermyContainer(blockEntity, id), new StringTextComponent(""))
     }
 }
